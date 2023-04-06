420deefb24022f70583fcb9d570b087a9616dcc1
Gabriel Rodríguez <gabrc52@gmail.com>
2023-04-06 01:56:24 -0400

    Room creation working (preliminary)!

--- matrix_receive.py
+++ matrix_receive.py
@@ -40,18 +40,25 @@ def process_events(txn_id):
 
 
 # https://spec.matrix.org/v1.6/application-service-api/#get_matrixappv1roomsroomalias
-@app.put('/rooms/<string:txn_id>')
-@app.put('/_matrix/app/v1/rooms/<string:txn_id>')
+# @app.put('/rooms/<string:room_alias>')
+# @app.put('/_matrix/app/v1/rooms/<string:room_alias>')
+# The endpoing above does NOT work, because of slashes in the mxid being confused for path slashes
+# The endpoint below DOES work
+zephyr_class_instance_mxid = f'#{config.zephyr_room_prefix}<string:cls>{config.class_instance_separator}<string:instance>:{config.homeserver}'
+@app.get(f'/rooms/{zephyr_class_instance_mxid}')
+@app.put(f'/_matrix/app/v1/rooms/{zephyr_class_instance_mxid}')
 @authenticated
-def create_room(room_alias):
-    print(f"Asked for room {room_alias}")
-    if not matrix.get_room_id(room_alias):
+# def create_room(room_alias):
+def create_room(cls, instance):
+    # print(f"Asked for room {room_alias}")
+    # if not matrix.get_room_id(room_alias):
+    if True: # TODO: fix lmao - actually check if it exists although the homeserver has no reason to call this if it exists
         # TODO: handle space creation for briding entire classes, etc
-        location = get_zephyr_location(room_alias)
-        if not location:
-            print(f"Returning 404, does not exist in Zephyr")
-            return {'errcode': 'edu.sipb.mit.not_implemented'}, 404
-        cls, instance = location
+        # location = get_zephyr_location(room_alias)
+        # if not location:
+            # print(f"Returning 404, does not exist in Zephyr")
+            # return {'errcode': 'edu.sipb.mit.not_implemented'}, 404
+        # cls, instance = location
         # TODO: actually subscribe to it (to do zephyr->matrix bridging)
         # keep track of subscriptions somewhere??! (yaml? json? sqlite?)
         # TODO: tweak room options for good user experience (history visibility, public, do we want federation? etc)
@@ -61,8 +68,10 @@ def create_room(room_alias):
             name=f'-c {cls} -i {instance}', # TODO: use a friendlier name
             preset='public_chat',
         )
-        assert room_id, '???' # TODO: handle
+        if not room_id:
+            return {'errcode': 'edu.sipb.mit.unknown'}, 500
         print("Done creating", cls, instance)
+    return {}
 
 
 # TODO: implement "thirdparty endpoints" (revert commit 4b12a963aa26f1f422eada928ac644dfcc394d87)
